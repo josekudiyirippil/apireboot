f9d9f2cc6ae7d8799fceff90f800bc56aaec8c9a
josekudiyirippil <70599126+josekudiyirippil@users.noreply.github.com>
2023-12-07 10:44:13 -0800

    Update delete-oldest-pod.sh

--- delete-oldest-pod.sh
+++ delete-oldest-pod.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
 
-# Get running pods starting with "queue-management-api"
-running_pods=$(oc get pods --field-selector=status.phase=Running --selector='app=queue-management-api' --output=jsonpath='{.items[*].metadata.name}' --sort-by=.metadata.creationTimestamp)
+# Get the count of pods that are not in the '0/1' state
+pod_count=$(oc get pods --selector=app=queue-management-api | awk '$2 != "0/1"'| wc -l)
 
-if [ -z "$running_pods" ]; then
-  echo "No running pods found for queue-management-api"
-  exit 1
+# Check if the pod count is less than 4
+if [ "$pod_count" -lt 5 ]; then
+    echo "Pod count not met"
+    exit 1
+else
+    # Get running pods starting with "queue-management-api"
+    running_pods=$(oc get pods --field-selector=status.phase=Running --selector='app=queue-management-api' --output=jsonpath='{.items[*].metadata.name}' --sort-by=.metadata.creationTimestamp)
+    
+    if [ -z "$running_pods" ]; then
+        echo "No running pods found for queue-management-api"
+        exit 1
+    fi
+
+	# Find the oldest pod
+	oldest_pod=$(echo "$running_pods" | awk '{print $1}')
+	echo "Deleting the oldest running pod: $oldest_pod"
+	oc delete pod "$oldest_pod"
 fi
-
-# Find the oldest pod
-oldest_pod=$(echo "$running_pods" | awk '{print $1}')
-echo "Deleting the oldest running pod: $oldest_pod"
-oc delete pod "$oldest_pod"
